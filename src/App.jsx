a21c73316fc4e515ee100757ae5bd1c372e11673
George Appianing <george.adu.appianing@gmail.com>
2025-03-18 16:16:15 +0000

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 function App() {
   return (
     <div>
-      <h2>Travel Away</h2>
+      <Logo />
+      <Form />
+      <PackinList />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>what do you need for your trip 😍</h3>
+    </div>
+  );
+}
+function PackinList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X %</em>
+    </footer>
   );
 }
 
